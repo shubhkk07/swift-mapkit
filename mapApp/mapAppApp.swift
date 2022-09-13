5ef9a1e21f8431c908a6e588a54d589b97e3acd4
shubhkk07 <shubhkk07@gmail.com>
2022-09-13 23:17:35 +0530

    Initial Commit

---
+++ mapApp/mapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapAppApp.swift
+//  mapApp
+//
+//  Created by Shubham Choudhary on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct mapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
